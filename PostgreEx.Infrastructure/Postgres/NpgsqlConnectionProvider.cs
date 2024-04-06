5cace73a06c5eed3a66a6c1c56f331ee89dacd4d
bymse <egorstrelov@gmail.com>
2024-04-06 22:29:20 +0300

    Npgsql connection and query executor

---
+++ PostgreEx.Infrastructure/Postgres/NpgsqlConnectionProvider.cs
@@ -0,0 +1,34 @@
+﻿using System.Data;
+using Npgsql;
+
+namespace PostgreEx.Infrastructure.Postgres;
+
+internal class NpgsqlConnectionProvider : IDisposable, IAsyncDisposable
+{
+    private readonly NpgsqlConnection connection;
+    
+    public NpgsqlConnectionProvider(string connectionString)
+    {
+        connection = new NpgsqlConnection();
+    }
+    
+    public async Task<NpgsqlConnection> Get()
+    {
+        if (connection.State != ConnectionState.Open)
+        {
+            await connection.OpenAsync();
+        }
+
+        return connection;
+    }
+
+    public void Dispose()
+    {
+        connection.Dispose();
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        await connection.DisposeAsync();
+    }
+}
---
+++ PostgreEx.Infrastructure/Postgres/NpgsqlQueryExecutor.cs
@@ -0,0 +1,42 @@
+﻿using Npgsql;
+
+namespace PostgreEx.Infrastructure.Postgres;
+
+internal class NpgsqlQueryExecutor(NpgsqlConnectionProvider connectionProvider)
+{
+    public async Task<T?> Execute<T>(string query) where T : class
+    {
+        var command = await GetCommand(query);
+        await using var reader = await command.ExecuteReaderAsync();
+        if (await reader.ReadAsync())
+        {
+            return await reader.GetFieldValueAsync<T>(0);
+        }
+
+        return null;
+    }
+
+    public async Task<Tuple<T1, T2>?> Execute<T1, T2>(string query)
+    {
+        var command = await GetCommand(query);
+        await using var reader = await command.ExecuteReaderAsync();
+        if (await reader.ReadAsync())
+        {
+            return new Tuple<T1, T2>(
+                await reader.GetFieldValueAsync<T1>(0),
+                await reader.GetFieldValueAsync<T2>(1)
+            );
+        }
+
+        return null;
+    }
+
+    private async Task<NpgsqlCommand> GetCommand(string query)
+    {
+        var connection = await connectionProvider.Get();
+        var command = connection.CreateCommand();
+        command.CommandText = query;
+
+        return command;
+    }
+}
