e295cc32ac436ee32ff1fd887058f29c4c8ac60d
bymse <egorstrelov@gmail.com>
2024-04-06 21:07:10 +0300

    Seq scan plan

---
+++ PostgreEx.Core/Plans/SeqScanPlan.cs
@@ -0,0 +1,21 @@
+﻿using PostgreEx.Core.Costs;
+
+namespace PostgreEx.Core.Plans;
+
+public class SeqScanPlan(
+    string relationName,
+    string alias,
+    PlanCosts costs,
+    Rows rows,
+    int width,
+    Plan[] plans
+) : Plan(
+    relationName,
+    alias,
+    costs,
+    rows,
+    width,
+    plans
+)
+{
+}
